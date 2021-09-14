685e3ab76cb08129e37ac18678506b63af94b3ef
siobhanlgorman <siobhanlgorman@gmail.com>
2021-09-14 14:31:17 +0000

    Add if statement to count number of even numbers in list and return True if no of evens is even and false if not

--- evens.py
+++ evens.py
@@ -10,7 +10,15 @@ def even_number_of_evens(numbers):
         if numbers == []:
             return False
         else:
-            return True
+            evens = 0
+            
+        for n in numbers:
+            if n % 2 == 0:
+                evens += 1
+        if evens:
+            return evens % 2 == 0
+        else:
+            return False
     else:
         raise TypeError("A list was not passed into the function")
 
