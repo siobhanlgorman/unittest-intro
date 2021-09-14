0186ceec7de04b9272ca80c250e24dfe0342de7d
siobhanlgorman <siobhanlgorman@gmail.com>
2021-09-14 14:15:20 +0000

    Fix function to pass test by adding if statement to check if value numbers is list else add string to putput when error raised

--- evens.py
+++ evens.py
@@ -6,8 +6,10 @@ def even_number_of_evens(numbers):
     if the number of even numbers is odd - return False
     if the numner of even numbers is even - return True
     """
-
-    return None
+    if isinstance(numbers, list):
+        return True
+    else:
+        raise TypeError("A list was not passed into the function")
 
 
 if __name__ == '__main__':
