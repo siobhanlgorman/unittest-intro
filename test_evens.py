835daee3580e87c72510487a35262f35bbba1120
siobhanlgorman <siobhanlgorman@gmail.com>
2021-09-14 08:59:00 +0000

    Import function into test file and set up basic failing test

--- test_evens.py
+++ test_evens.py
@@ -1,8 +1,9 @@
 import unittest
-
+from evens import even_number_of_evens
 
 class TestEvens(unittest.TestCase):
-    pass
+    def test_function_returns_True(self):
+        self.assertTrue(even_number_of_evens([]))
 
 
 unittest.main()
