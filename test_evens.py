0e8c54bf48c15a5f74f9abf278cceb4e7b579870
siobhanlgorman <siobhanlgorman@gmail.com>
2021-09-14 14:08:56 +0000

    Write failing function to test if typeerror is raised when function is called and value is 4

--- test_evens.py
+++ test_evens.py
@@ -3,7 +3,8 @@ from evens import even_number_of_evens
 
 
 class TestEvens(unittest.TestCase):
-    pass
+    def test_throws_error_if_value_passed_in_is_not_list(self):
+        self.assertRaises(TypeError, even_number_of_evens, 4)
 
 
 if __name__ == '__main__':
